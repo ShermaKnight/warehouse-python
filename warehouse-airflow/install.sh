d1663a3fa08e04a40d3343a37f54659fe2cffd41
vmartinir <vmartinir@aliyun.com>
2022-09-12 10:51:27 +0800

    Update airflow module

---
+++ warehouse-airflow/install.sh
@@ -0,0 +1,5 @@
+python3 -m pip install --upgrade pip
+
+pip3 install "apache-airflow[celery]==2.3.4" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.3.4/constraints-3.7.txt"
+
+
---
+++ warehouse-airflow/tutorial.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
